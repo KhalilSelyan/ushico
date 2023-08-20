c0f5800117eeb842468bbfb905a40550bb483ed2
Khalil Selyan <khalilselyan2@gmail.com>
2023-08-21 02:57:39 +0300

    helpers

---
+++ src/helpers/getfriendsbyid.ts
@@ -0,0 +1,29 @@
+import { fetchRedis } from "./redis";
+
+export async function getFriendsById(id: string) {
+  const friendIds = (await fetchRedis(
+    "smembers",
+    `user:${id}:friends`
+  )) as string[];
+
+  const friends = await Promise.allSettled(
+    friendIds.map(async (friendId) =>
+      JSON.parse(await fetchRedis("get", `user:${friendId}`))
+    )
+  );
+
+  const friendsFailed = friends.filter(
+    (friend) => friend.status === "rejected"
+  );
+
+  const friendsResolved = friends
+    .filter(
+      (friend): friend is PromiseFulfilledResult<User> =>
+        friend.status === "fulfilled"
+    )
+    .map((friend) => {
+      return friend.value;
+    });
+
+  return friendsResolved;
+}
