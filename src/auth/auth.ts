3bf4f3ebdfffc6a1dacc655955798a5545d070c3
Khalil Selyan <khalilselyan2@gmail.com>
2025-12-14 16:19:13 +0300

    fix: add trusted origins for dev and prod

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -2,11 +2,16 @@ import db from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import * as schema from "@/db/schema";
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     schema,
   }),
+  trustedOrigins: [
+    "http://localhost:3000",
+    "https://ushico.khalilselyan.com",
+  ],
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
